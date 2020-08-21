063c2dbd7c09b9f078a7856c90240737e47c66de
OzzyCodes <ajwaldro@gmail.com>
2020-08-21 16:06:23 -0400

    updates

--- run_macro_with_python.py
+++ run_macro_with_python.py
@@ -6,22 +6,19 @@ if os.path.exists("excelsheet.xlsm"):
     xl = win32com.client.Dispatch("Excel.Application")
     xl.Workbooks.Open(os.path.abspath("excelsheet.xlsm"), ReadOnly=1)
     xl.Application.Run("excelsheet.xlsm!modulename.macroname")
-    """
-    xl.Application.Save() # if you want to save then uncomment this line and change delete the ", ReadOnly=1" part from the open function.
-    """
+    """xl.Application.Save() # if you want to save then uncomment this line and change delete the ", ReadOnly=1" part 
+    from the open function. """
     xl.Application.Quit()  # Comment this out if your excel script closes
 
     del xl
-
 
 # Import the following library to make use of the DispatchEx to run the macro
 import win32com.client as wincl
 import os.path
 
+
 def runMacro():
-
     if os.path.exists("C:\\Users\\Dev\\Desktop\\Development\\completed_apps\\My_Macr_Generates_Data.xlsm"):
-
         # DispatchEx is required in the newest versions of Python.
         excel_macro = wincl.DispatchEx("Excel.application")
         excel_path = os.path.expanduser(
